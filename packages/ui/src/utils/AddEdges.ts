9793e186917aa7ae2202af5e286757ed3e8e3e6b
PorcoRosso85 <1.is.universe@gmail.com>
2023-09-08 09:22:56 +0900

    type of param, done extracted

--- packages/ui/src/utils/AddEdges.ts
+++ packages/ui/src/utils/AddEdges.ts
@@ -2,5 +2,5 @@ import Graph from "graphology";
 
 export const addingEdge = (graph: Graph) => {
   // エッジを追加
-  graph.addEdge("n1", "n2", { id: "e1" });
+  graph.addEdge("n1", 2, { id: "e1" });
 };
--- packages/ui/src/utils/AddNodes.ts
+++ packages/ui/src/utils/AddNodes.ts
@@ -1,17 +1,28 @@
 import Graph from "graphology";
+import AbstractGraph from "graphology-types";
+
+type AddNodeParams = Parameters<AbstractGraph["addNode"]>;
 
 export const addingNode = (graph: Graph) => {
-  // ノードを追加
-  graph.addNode("n1", {
-    label: "Node 1",
-    x: Math.random(),
-    y: Math.random(),
-    size: 5,
-  });
-  graph.addNode("n2", {
-    label: "Node 2",
-    x: Math.random(),
-    y: Math.random(),
-    size: 5,
-  });
+  const params01: AddNodeParams = [
+    "n1",
+    {
+      label: "Node 1",
+      x: Math.random(),
+      y: Math.random(),
+      size: 5,
+    },
+  ];
+  graph.addNode(...params01);
+
+  const params02: AddNodeParams = [
+    2,
+    {
+      label: "Node 2",
+      x: Math.random(),
+      y: Math.random(),
+      size: 5,
+    },
+  ];
+  graph.addNode(...params02);
 };
