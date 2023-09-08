65a13a70d22c5cc7cffc6a602a37085e168d58f0
PorcoRosso85 <1.is.universe@gmail.com>
2023-09-08 09:50:19 +0900

    addedgeparams, contract of type

--- packages/ui/src/utils/AddEdges.ts
+++ packages/ui/src/utils/AddEdges.ts
@@ -1,6 +1,7 @@
 import Graph from "graphology";
+import { AddEdgeParams } from "../interface/AddEdgeParams";
 
 export const addingEdge = (graph: Graph) => {
-  // エッジを追加
-  graph.addEdge("n1", 2, { id: "e1" });
+  const params: AddEdgeParams = ["n1", 2, { id: "e1" }];
+  graph.addEdge(...params);
 };
