f2f60f05c03ee0814493cf06d43769b4e93a1f5b
PorcoRosso85 <1.is.universe@gmail.com>
2023-09-11 16:50:48 +0900

    integrate react, radixUi, shadcn for components

--- packages/ui/src/pages/GraphView.ts
+++ packages/ui/src/pages/GraphView.ts
@@ -1,10 +1,4 @@
-import Sigma from "sigma";
-import Graph from "graphology";
-
-import { addingNode } from "../utils/AddNodes";
-import { addingEdge } from "../utils/AddEdges";
 import { container } from "../utils/gettingContainer";
-import { SelectorDefinitions } from "../utils/SelectorDefinitions";
 import { graphInstance } from "../service/GraphInstance";
 import { rendererInstance } from "../service/RendererInstance";
 
---
+++ packages/ui/src/pages/readme.ts
@@ -0,0 +1,6 @@
+const readme = {
+  1: {
+    integrations:
+      "https://docs.astro.build/en/guides/migrate-to-astro/from-nextjs/",
+  },
+}
